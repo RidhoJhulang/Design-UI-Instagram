18f557a72cc0a16f3ea35da708e3cc6187e1bf86
RidhoJhulang <ridho_jhulang_26rpl@student.smktelkom-mlg.sch.id>
2019-07-05 16:47:12 +0700

    Calculating Followers Count and Following Count

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,7 +9,7 @@
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <div class="d-flex align-items-center pb-2">
-                    
+
                      <div class="h4">{{ $user->username }}</div>
                      
                 <follow-button user-id="{{ $user->id }}" follows="{{ $follows }}"></follow-button>
@@ -30,8 +30,8 @@
 
             <div class="d-flex">
             <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
-            <div class="pr-5"><strong>303k</strong> followers</div>
-            <div class="pr-5"><strong>2</strong> following</div>
+            <div class="pr-5"><strong>{{ $user->profile->followers->count() }}</strong> followers</div>
+            <div class="pr-5"><strong>{{ $user->following->count() }}</strong> following</div>
         </div>
         <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
 
